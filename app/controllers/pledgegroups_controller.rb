ca8e65a44580a1af2a8b59c4287e74da822a68b4
Arthur Wu <warthur123@gmail.com>
2022-04-13 23:23:32 -0400

    Added ability to update pledgegroups and put pledgegroup form in separate view

--- app/controllers/pledgegroups_controller.rb
+++ app/controllers/pledgegroups_controller.rb
@@ -17,4 +17,21 @@ class PledgegroupsController < ApplicationController
       redirect_back fallback_location: new_pledgegroup_path
     end
   end
+
+
+
+  def edit
+    @pledgegroup = Pledgegroup.find(params[:id])
+  end
+
+  def update
+    @pledgegroup = Pledgegroup.find(params[:id])
+
+    if @pledgegroup.update({ budget: params[:pledgegroup][:budget], admins: [current_user], is_public: (params[:is_public] ? true : false)})
+      redirect_to pledgegroups_url
+    else
+      flash[:error] = "Pledge group could not be saved with the given input"
+      redirect_back fallback_location: edit_pledgegroup_path
+    end
+  end
 end
